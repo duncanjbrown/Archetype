2eef16c4818f4e2feb0c4200c6e1982f5d40558f
Duncan Brown <duncanjbrown@gmail.com>
2013-02-04 16:01:42 +0000

    fix funnel to be simpler

--- archetype.funnel.php
+++ archetype.funnel.php
@@ -4,6 +4,8 @@
  * @subpackage funnel
  */
 
+define( 'AT_FUNNEL_META', 'at_funnel_progress' );
+
 /**
  * Pass the user through a series of pages so they must visit them in order.
  *
@@ -22,41 +24,24 @@ class Archetype_Funnel {
 	 */
 	protected $user;
 
-	protected $name;
-
-	/**
-	 * Register a user as having completed the signup process
-	 * This should be called from a handler accepting the user's 
-	 * completion of the process, eg a submit saying 'I'm done'.
-	 * 
-	 * Or you could complete them in a phase callback, below.
-	 *
-	 * @param User $user
-	 * @param string $path where to send them on completion
-	 */
-	public static function complete( $user, $path = '/', $redirect = true ) {
-		
-		$user->register_signup_completed();
-
-		wp_redirect( '/' );
-		die();
-
-		return;
-	}
-
 	/**
 	 * Pass in a user who hasn't finished signup.
 	 *
-	 * This method takes the phases defined in $this->get_phases()
+	 * This method takes in the phases from the second param
 	 * and subjects the user to each of their test cases.
 	 * In the event that a user fails a test, that test
 	 * is allowed to execute its callback.
 	 *
-	 * @param User $user the user to test
+	 * @param array $phases the phases to go through
+	 * @param array $opts other settings
 	 */
-	function __construct( User $user ) {
+	function __construct( $phases = array(), $opts = array() ) {
 
-		$this->user = $user;
+		$this->user = User::current_user();
+		$this->phases = $phases;
+
+		$this->opts = wp_parse_args( $opts, array(
+			'meta_key' => AT_FUNNEL_META ) );
 
 		// get the first phase which the user fails (eg the one they need to do next)
 		$next = $this->get_next_phase();
@@ -65,26 +50,12 @@ class Archetype_Funnel {
 		if( !$next )
 			return;
 
-		// we may want to check if we're on such a page in future
+		// we may want to check if we're on such a page in future - give em the funnel name if so
 		add_filter( 'at_is_funnel', '__return_true' );
 
 		// main bit: if there is a phase, hand control to it
-		$phases = $this->get_phases();
-		$this->current_phase = $phases[$next];
-		add_action( $this->current_phase['hook'], $this->current_phase['callback'] );
-
-		// add some data to the ri_signup_process_page hook so the user knows where they are
-		//add_action( 'at_funnel_process_page', array( &$this, 'display_progress' ) );
-	}
-
-	/**
-	 * Show the user's progress through the signup process
-	 *
-	 * @return void
-	 */
-	public function display_progress() {
-		$phases = $this->get_phases();
-		hm_get_template_part( 'signup/progress_bar', array( 'phases' => $phases, 'current_phase' => $this->current_phase, 'phase_count' => count( $phases ) ) );
+		$this->current_phase = $next;
+		add_action( $this->current_phase->hook, $this->current_phase->callback );
 	}
 
 	/**
@@ -95,103 +66,79 @@ class Archetype_Funnel {
 	 */
 	protected function get_next_phase() {
 
-		$tests = $this->get_tests();
-
-		foreach ( $tests as $phase => $test ) {
-
-			if ( call_user_func( $test, $this->user ) === false ) {
-				return $phase; // if they fail the test
-			}
+		foreach( $this->phases as $phase ) {
+			if ( ! $phase->do_test( $this->user, $this->opts['meta_key'] ) )
+				return $phase;
 		}
 
 		return false;
 	}
 
-	/**
-	 * Get the test function for each of the phases described in get_phases, below.
-	 *
-	 * @return array of functions
-	 * @access private
-	 */
-	protected function get_tests() {
 
-		$tests = array();
+}
 
-		foreach ( $this->get_phases() as $phase => $data ) {
-			switch ( $data['test_type'] ) {
+class Archetype_Signup_Phase {
 
-			case 'usermeta' :
-				$tests[$phase] = $this->create_usermeta_test( $data );
-				break;
+	var $slug;
+	var $title;
+	var $short_title;
+	var $callback;
+	var $hook;
+	var $test;
 
-			case 'once' :
-				$name = $this->name;
-				$tests[$phase] = function( $user ) use ( $phase, $name ) {
-					if( !$progress = $user->get_meta( $name, true ) ) {
-						$progress = array();
-					}
+	function __construct( $options ) {
 
-					return in_array( $phase, $progress );
-				};
-				break;
+		$options = wp_parse_args( $options, array( 
+			'title' => null,
+			'short_title' => null,
+			'slug' => null,
+			'hook' => 'template_redirect',
+			'callback' => array( $this, 'redirect_to_self' ),
+			'test' => array( $this, 'test_seen_once' ) 
+		) );
 
-			}
+		if( !$options['short_title'] )
+			$options['short_title'] = $options['title'];
+
+		if( !$options['slug'] )
+			throw new Exception( 'Missing slug in signup phase' );
+
+		foreach( $options as $name => $opt ) {
+			$this->{$name} = $opt;
 		}
-		return $tests;
 	}
 
-	/**
-	 * Pile up phases here in the order they should be completed by the user
-	 *
-	 *
-	 * @access protected
-	 * @return array
-	 */
-	protected function get_phases() {
-
-		/* 
-			EXAMPLE
-		$phases = array(
-			'tour' => array(
-				'name' => 'Take a tour',
-				'shortname' => 'Tour',
-				'test_type' => 'usermeta',
-				'meta_key' => RI_TOUR_META,
-				'hook' => 'template_redirect',
-				'callback' => function() use ( $template ) {
-
-					global $wp_query;
-
-					if ( !get_query_var( 'is_tour' ) ) {
-						wp_redirect( site_url( $template->get_url_for( 'tour' ) ) );
-						die();
-					}
-				}
-			)
-
-			return $phases;
-		*/
-	
+	function do_test( $user, $meta_key ) {
+		return call_user_func_array( $this->test, array( $user, $meta_key ) );
 	}
 
-	/**
-	 * Generate a test function for a usermeta value
-	 *
-	 * @return callable the function to test ( returns true or false )
-	 */
-	private function create_usermeta_test( $phase ) {
+	function test_seen_once( $user, $meta_key ) {
+		
+		$seen = $user->get_meta( $meta_key, true );
 
-		return function( $user ) use( $phase ) {
-			if ( $user->get_meta( $phase[ 'meta_key' ], true ) ) {
-				return true;
-			}
-			return false;
-		};
+		if( !is_array( $seen ) ) {
+			$seen = array();
+		}
+
+		if( in_array( $this->slug, $seen ) ) {
+			return true;
+		}
+
+		array_push( $seen, $this->slug );
+		$user->update_meta( $meta_key, $seen );
+
+		return false;
 	}
+
+	function redirect_to_self() {
+		wp_redirect( get_permalink( get_page_by_path( $this->slug ) ) );
+		die();
+	}
+
 }
 
 /**
- * Are we on a signup process page?
+ * Are we on a funnel page?
  * @return bool
  */
 function at_is_funnel() {
